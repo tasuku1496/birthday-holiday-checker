1c2d7f3c26600a3f777edc33a1e476dbaeee0d09
tasuku1496 <tishiguro@careerindex.co.jp>
2025-02-07 09:28:31 +0900

    fix: disable状態の考慮を追加

--- src/components/CustomButton.tsx
+++ src/components/CustomButton.tsx
@@ -5,24 +5,26 @@ type CustomButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
   className?: string;
 };
 
-const CustomButton: React.FC<CustomButtonProps> = ({
+const CustomButton = ({
   variantType = "blue",
   className = "",
   children,
+  disabled,
   ...rest
-}) => {
+}: CustomButtonProps) => {
   const baseClasses =
     "font-kaiseiTokumin text-base normal-case rounded py-2 px-4 transition-colors duration-200";
 
-  const variantClasses =
-    variantType === "blue"
-      ? "bg-blue-500 text-white hover:bg-blue-700"
-      : "bg-gray-500 text-black hover:bg-gray-700";
+  const variantClasses = disabled
+    ? "bg-gray-300 text-gray-600 cursor-not-allowed"
+    : variantType === "blue"
+    ? "bg-blue-500 text-white hover:bg-blue-700"
+    : "bg-gray-500 text-black hover:bg-gray-700";
 
   const combinedClasses = `${baseClasses} ${variantClasses} ${className}`;
 
   return (
-    <button className={combinedClasses} {...rest}>
+    <button className={combinedClasses} disabled={disabled} {...rest}>
       {children}
     </button>
   );
