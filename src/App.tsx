c2bd5d724c7734153639c160af177a579751e2ea
tasuku1496 <tishiguro@careerindex.co.jp>
2025-02-17 09:56:03 +0900

    fix: h1文言を修正

--- src/App.tsx
+++ src/App.tsx
@@ -52,7 +52,7 @@ const BirthdayHolidayChecker = () => {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-2xl font-bold mb-4">誕生日休日チェッカー</h1>
+      <h1 className="text-2xl font-bold mb-4">お誕生日休日チェッカー</h1>
       <p className="mb-4">これからの誕生日が平日か土日かを教えてあげます</p>
       <div className="mb-4 flex space-x-4 items-center">
         <MonthDaySelect type="month" value={month} setValue={setMonth} />
